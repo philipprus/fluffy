eb001c6125dbad62f586b2e6d9aa2e7a205b1983
Philipp Prus <philipp@prcptv.ai>
2019-09-23 00:32:16 +0300

    console.log proccess.env

--- server/store/connection.js
+++ server/store/connection.js
@@ -3,7 +3,7 @@ const {get, KnownConfigKey } = require('../utils/config');
 
 const { MongoConnection } = require('../utils/mongo-connection');
 
-const url = get(KnownConfigKey.DbServer, 'mongodb://localhost:27017/fluffy');
+const url = "mongodb+srv://fl_user_123:mUt6%6v2meu57_C@cluster0-di3uu.mongodb.net/db_fluffy?retryWrites=true&w=majority" || get(KnownConfigKey.DbServer, 'mongodb://localhost:27017/fluffy');
 
 const connection = new MongoConnection(url);
 
