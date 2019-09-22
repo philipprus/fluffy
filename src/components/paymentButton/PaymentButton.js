5800aea8dd188867a0e6ebad2184b743576b47a5
Philipp Prus <philipp@prcptv.ai>
2019-09-22 18:23:16 +0300

    change package

--- src/components/paymentButton/PaymentButton.js
+++ src/components/paymentButton/PaymentButton.js
@@ -1,27 +1,27 @@
-import { PayPalButton } from "react-paypal-button-v2";
+// import { PayPalButton } from "react-paypal-button-v2";
 import React from 'react';
 
 const PaymentButton = (props) => {
-    return (
-      <PayPalButton
-        // amount="0.01"
-        amount={props.total}
-        currency="ILS"
-        options={{clientId: "sb"}}
-        catchError={(err)=> {alert(err);}}
-        onCancel={(data)=> {alert(data);}}
-        onSuccess={(details, data) => {
-          alert("Transaction completed by " + details.payer.name.given_name);
+    return (<></>
+      // <PayPalButton
+      //   // amount="0.01"
+      //   amount={props.total}
+      //   currency="ILS"
+      //   options={{clientId: "sb"}}
+      //   catchError={(err)=> {alert(err);}}
+      //   onCancel={(data)=> {alert(data);}}
+      //   onSuccess={(details, data) => {
+      //     alert("Transaction completed by " + details.payer.name.given_name);
 
-          // OPTIONAL: Call your server to save the transaction
-          return fetch("/paypal-transaction-complete", {
-            method: "post",
-            body: JSON.stringify({
-              orderID: data.orderID
-            })
-          });
-        }}
-      />
+      //     // OPTIONAL: Call your server to save the transaction
+      //     return fetch("/paypal-transaction-complete", {
+      //       method: "post",
+      //       body: JSON.stringify({
+      //         orderID: data.orderID
+      //       })
+      //     });
+      //   }}
+      // />
     );
   }
 
