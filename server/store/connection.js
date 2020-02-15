144d6a1bebc1bc9bf51a4cf643a45cbecb8b2833
Philipp Prus <philipp@prcptv.ai>
2020-02-15 19:36:56 +0200

    sec key

--- server/store/connection.js
+++ server/store/connection.js
@@ -3,7 +3,7 @@ const {get, KnownConfigKey } = require('../utils/config');
 
 const { MongoConnection } = require('../utils/mongo-connection');
 
-const url = process.env.NODE_ENV !== 'production' ? 'mongodb+srv://fl_temp_user:qwerty12345@cluster0-di3uu.mongodb.net/test?retryWrites=true&w=majority' : "mongodb://heroku_59x1b5dl:SqG!7iZ9UuRkXns@ds115434.mlab.com:15434/heroku_59x1b5dl";
+const url = process.env.NODE_ENV !== 'production' ? 'mongodb+srv://fl_temp_user:qwerty12345@cluster0-di3uu.mongodb.net/test?retryWrites=true&w=majority' : process.env.MONGODB_URI;
 
 const connection = new MongoConnection(url);
 
